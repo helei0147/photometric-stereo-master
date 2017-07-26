3f09988f3f63ea76d7a89f2498f2e5e22ba6e222
URefrain <helei0147@163.com>
2017-07-26 15:22:19 +0800

    give an vectorized image, set the median to png pixel value 127, return the autoexposed im(vectorized) and the exposure ratio

---
+++ auto_exposure.m
@@ -0,0 +1,20 @@
+function [ auto_exposed_im, exposure_scale ] = auto_exposure( im, curve_matrix)
+%AUTO_EXPOSURE 此处显示有关此函数的摘要
+%   此处显示详细说明
+    for channel = 1:3
+        mask = im(:,channel)>0;
+        valid = im(:,channel);
+        valid = valid(mask);
+        k_(channel) = median(valid);
+    end
+    [k, index] = max(k_);
+    curve_resolution = size(curve_matrix,2);
+    center = floor(curve_resolution/2);
+    center_value = curve_matrix(:,center)';
+    scale = center_value(index)/k; %曝光时间相对值
+    im = im*scale;
+    ldr = to_ldr_vec(im,curve_matrix);
+    auto_exposed_im = ldr;
+    exposure_scale = scale;
+end
+
