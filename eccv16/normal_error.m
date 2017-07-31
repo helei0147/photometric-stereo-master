3ca06d85548fc3a762aa52302665bc033fa7969e
URefrain <helei0147@163.com>
2017-07-31 22:23:07 +0800

    calculate average normal error of the two parameters. Returns vector of error in degree.

--- eccv16/normal_error.m
+++ eccv16/normal_error.m
@@ -1,4 +1,5 @@
 function [error] = normal_error(normal1, normal2)
     to_acos = sum(normal1.*normal2,2);
     error = acos(to_acos)/pi*180;
+    fprintf(sprintf('error:%f\n',sum(error)/size(error,1)));
 end
