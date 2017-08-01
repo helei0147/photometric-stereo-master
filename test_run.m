f2d1f027e11695d1fd70e492eb3b6906dcc35e25
URefrain <helei0147@163.com>
2017-08-01 12:24:47 +0800

    five materials, test TPAMI in random exposure

---
+++ test_run.m
@@ -0,0 +1,12 @@
+light_number = 25;
+counter = 1;
+for i = [99, 59, 19, 85, 73]
+    
+    image_path=sprintf('rabbit_all/lights_%d/hdr/%d_6',light_number,i);
+    light_file=sprintf('lights_%d.txt',light_number);
+    mat_file = 'rabbit.mat';
+    isldr = 0; 
+    is_ae = 0;
+    [error_hdr(counter),~, hdr_nan(counter)] = cal_n_pixelwise_without_optimize(image_path,light_file,mat_file,isldr,is_ae);
+    counter = counter+1;
+end
