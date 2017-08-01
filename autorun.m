0be2924ebc9452def0d98e36e62e3f682d63925e
URefrain <helei0147@163.com>
2017-08-01 08:23:09 +0800

    calculate the difference between auto-exposure and hdr.auto expusure assume exposure time is known

--- autorun.m
+++ autorun.m
@@ -1,10 +1,12 @@
+light_number = 89;
 exposure = zeros(89,100);
 for i = 0:99
-    image_path=sprintf('rabbit_all/lights_89/hdr/%d_6',i);
-    light_file='lights_89.txt';
+    image_path=sprintf('rabbit_all/lights_%d/hdr/%d_6',light_number,i);
+    light_file=sprintf('lights_%d.txt',light_number);
     mat_file = 'rabbit.mat';
     isldr = 0; 
     [error_hdr(i+1),~, hdr_nan(i+1)] = cal_n_pixelwise_without_optimize(image_path,light_file,mat_file,isldr,0);
     [error_ae(i+1), exposure(:,i+1), ae_nan(i+1)] = cal_n_pixelwise_without_optimize(image_path,light_file,mat_file,isldr,1);
 end
 save('exposure_89.mat','exposure');
+save(sprintf('is_nan_%d',light_number),'ae_nan','hdr_nan');
