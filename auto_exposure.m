f657972cf6465c576aae16ce460722bcba3ada12
URefrain <helei0147@163.com>
2017-08-02 16:19:14 +0800

    set pixel value at 75% to the middle, scaling on the whole image

--- auto_exposure.m
+++ auto_exposure.m
@@ -1,11 +1,17 @@
 function [ auto_exposed_im, exposure_scale ] = auto_exposure( im, curve_matrix)
 %AUTO_EXPOSURE 此处显示有关此函数的摘要
 %   此处显示详细说明
+%     for channel = 1:3
+%         mask = im(:,channel)>0;
+%         valid = im(:,channel);
+%         valid = valid(mask);
+%         k_(channel) = median(valid);
+%     end
+    pixel_num = size(im,1);
+    middle_percent = 0.75;
     for channel = 1:3
-        mask = im(:,channel)>0;
-        valid = im(:,channel);
-        valid = valid(mask);
-        k_(channel) = median(valid);
+        sorted = sort(im(:,channel));
+        k_(channel) = sorted(uint32(middle_percent*pixel_num));
     end
     [k, index] = max(k_);
     curve_resolution = size(curve_matrix,2);
