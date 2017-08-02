afe266f04e01535d661d40b0b93c39e628642192
URefrain <helei0147@163.com>
2017-08-02 16:22:09 +0800

    adding nan and light_less_than_three flag, cut off error bigger than 90 and set to 90

--- cal_n_pixelwise_without_optimize.m
+++ cal_n_pixelwise_without_optimize.m
@@ -12,7 +12,7 @@ mat_file = sprintf('data/mats/%s',mat_file);
 iter_max=100;
 %     load mask
 load(sprintf('%s',mat_file));
-mask = uint8(mask);
+mask = mask>0;
 v_ind=find(mask>0);
 %     load light source info
 lights = load(light_file);
@@ -92,12 +92,14 @@ if is_ae
     e_matname = sprintf('%s/exposure_scale.mat',ae_path);
     save(e_matname,'exposure_scale');
 else
-%     [origin_I, exposure] = random_exposure(origin_I,0.08);
+%     load(sprintf('random_%d.mat',light_number));
+%     [origin_I, exposure] = random_exposure(origin_I,0.33,random_vec);
 end
 % total_median = median(origin_I(:));
 % for i = 1:light_number
 %     origin_I(:,i) = auto_exposure(origin_I(:,i),total_median);
 % end
+
 for i = 1:light_number
     gs_img = origin_I(:,i);
     threshold_percent = 0.3;% pixels remain
@@ -105,6 +107,7 @@ for i = 1:light_number
     zero_num = size(find(sorted_img<1e-6),1);
     p_num = size(sorted_img,1);
     threshold = sorted_img(uint32((p_num-zero_num)*threshold_percent)+zero_num);
+    total_threshold(i) = threshold;
     gs_img(gs_img>threshold)=-1;
     I(:,i)=gs_img;
 end
@@ -115,11 +118,19 @@ para_num=9;
 parameter_buffer=zeros(valid_pixel_count,para_num);
 error_buffer = zeros(valid_pixel_count,1);
 t0 = cputime;
+not_valid_counter = 1;
+zero_counter = 0;
+one_counter = 0;
+two_counter = 0;
+three_counter = 0;
 for i=1:size(I,1)
     if mod(i,1000)==0
         fprintf('pixel:%d, used up %f s\n',i,cputime-t0);
     end
     buffer=I(i,:);
+    if i==649
+        a = 1+1;
+    end
     buffer_mask=buffer>0; % a row mask
     valid_buffer=buffer(buffer_mask)'; % a column 
     valid_light=light_true(buffer_mask',:); % first dimension is light index, second dimension is x, y and z
@@ -127,27 +138,90 @@ for i=1:size(I,1)
 %   first time for photometric stereo, get a initial value for the normal
 %   of this pixel
     L=valid_light;
+    
+    valid_L_num(i) = size(L,1);
     intense=valid_buffer;
-    n = (L.'*L)\(L.'*intense);
+    
+    switch(size(L,1))
+        case 0
+            zero_counter = zero_counter+1;
+        case 1
+            one_counter = one_counter+1;
+        case 2
+            two_counter = two_counter+1;
+        otherwise
+            three_counter = three_counter+1;
+    end
+    A = L.'*L;
+    b = L.'*intense;
+    n = A\b;
     
 %     normalize the normal of this pixel
-    n=n/norm(n);
-    
-
+    n=n/norm(n); 
     normal_matrix(i,:)=n';
     pixel_error = acos(nn(i,:)*n)/pi*180;
     error_buffer(i) = pixel_error;
+    
+%     if (pixel_error<1)
+%         disp(pixel_error)
+%         L
+%         plot3(L(:,1),L(:,2),L(:,3),'ro');
+%         axis equal
+% %     end
+%     if (pixel_error>15)&&(size(L,1)>=4)
+%         disp(pixel_error)
+%         L
+%         plot3(L(:,1),L(:,2),L(:,3),'ro');
+%         axis equal
+%     end
+%     if size(find(isnan(n)>0),1)>0
+%         if size(L,1)>3
+%             
+%             disp(size(L,1));
+%             disp(L);
+%             l1 = cross(L(1,:),L(2,:));
+%             res = dot(l1,L(3,:));
+%             disp(res);
+%             disp_pos(mask,i);
+%         end
+%     end
+%     if size(find(isnan(n)>0),1)>0
+%         disp(L);
+%     end
+
 %     fprintf('pixel %d, error %f\n',i,pixel_error);
 %     fprintf('parameter error:%f\n',para_error);
 %     fprintf('iter: %d\n',iter);
 end
-show_normal(normal_matrix,mask,image_path);
+fprintf('%d %d %d %d',zero_counter,one_counter,two_counter,three_counter);
+nan_size = size(find(isnan(normal_matrix(:,1))>0),1);
+nan_mask = isnan(normal_matrix(:,1))>0;
+C = [valid_L_num',error_buffer];
+normal_matrix = interp_nan(normal_matrix);
+% show_normal(normal_matrix,mask,image_path);
 cos_error_vector= sum(normal_matrix.*nn,2);
-nan_size = size(find(isnan(cos_error_vector)),1);
-cos_error_vector(isnan(cos_error_vector))=1;
+%------------------------------------------
+without_nan = 1;
+without_light_less_than_three_flag = 1;
+%------------------------------------------
+pixel_in_count = valid_pixel_count;
 
-norm_degree_error = sum(acos(cos_error_vector)/pi*180)/valid_pixel_count
+if without_nan==1
+    cos_error_vector(nan_mask) = 1;
+    pixel_in_count = pixel_in_count-nan_size;
+end
 
+light_less_than_three = size(find(C(:,1)<3),1);
+if without_light_less_than_three_flag==1
+    cos_error_vector(C(:,1)<3) = 1; % 光不足3个的去除计算
+    pixel_in_count = pixel_in_count-light_less_than_three;
+end
+
+% set normal error bigger than 90 to 90
+cos_error_vector(cos_error_vector<0) = 0;
+
+norm_degree_error = sum(acos(cos_error_vector)/pi*180)/pixel_in_count;
+% norm_degree_error = sum(acos(cos_error_vector)/pi*180)/valid_pixel_count
 % [pic_height,pic_width]=size(mask);
 % first_dim_vector=floor(v_ind/pic_height)+1;
 % second_dim_vector=mod(v_ind,pic_height)+1;
