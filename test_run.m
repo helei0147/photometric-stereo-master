abd50481e5ec33a9e1a01ac07e765ade0be86e08
URefrain <helei0147@163.com>
2017-08-02 16:23:14 +0800

    modifying for running the script, exactly nothing

--- test_run.m
+++ test_run.m
@@ -1,9 +1,10 @@
 light_number = 25;
 counter = 1;
 for i = [99, 59, 19, 85, 73]
-    
-    image_path=sprintf('rabbit_all/lights_%d/hdr/%d_6',light_number,i);
-    light_file=sprintf('lights_%d.txt',light_number);
+%     image_path=sprintf('rabbit_all/lights_%d/hdr/%d_6',light_number,i);
+%     light_file=sprintf('lights_%d.txt',light_number);
+    image_path=sprintf('rabbit_25L_new/%d_6',i);
+    light_file='lights_250.txt';
     mat_file = 'rabbit.mat';
     isldr = 0; 
     is_ae = 0;
