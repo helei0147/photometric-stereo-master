5b41180b290a208d00df1b6db498b08fc36b9160
URefrain <helei0147@163.com>
2017-08-02 20:34:23 +0800

    use linear joint method in eccv16, get normal and exposure time, display normal map

--- eccv16/test_linear_joint.m
+++ eccv16/test_linear_joint.m
@@ -1,40 +1,39 @@
-[I,L] = public(73);
-% mask_for_I = [3,5,6,7,9:19,21,23,25];
-% I = I(:,mask_for_I);
-% L = L(mask_for_I,:);
-light_number = size(L,1);
-pixel_number = size(I,1);
-y = linear_joint_dense(I,L);
-e = y(pixel_number*3+1:end);
-e = e.^-1;
-b = y(1:pixel_number*3);
-if sum(e)<0
-    e = -e;
-    b = -b;
+function [pixel_error, avg_error ] = test_linear_joint( material_index)
+    [I,L] = public(material_index);
+    light_number = size(L,1);
+    pixel_number = size(I,1);
+    y = linear_joint_dense(I,L);
+    e = y(pixel_number*3+1:end);
+    e = e.^-1;
+    b = y(1:pixel_number*3);
+    if sum(e)<0
+        e = -e;
+        b = -b;
+    end
+    n_ = reshape(b,3,[])';
+    len = sum(n_.*n_,2);
+    len = len.^0.5;
+    n = n_./[len len len];
+
+    load('rabbit_small.mat');
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % n = -n;
+    % n(:,3) = -n(:,3);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    zz = zeros(size(nn2));
+    temp = zeros(size(nn2,1),size(nn2,2));
+    temp(mask) = n(:,1);zz(:,:,1) = temp;
+    temp(mask) = n(:,2);zz(:,:,2) = temp;
+    temp(mask) = n(:,3);zz(:,:,3) = temp;
+    figure(1);
+    imagesc(uint8((zz+1)*128));
+    title('RGB Normal Map');
+    axis equal;
+    figure(2);
+    imagesc(uint8((nn2+1)*128));
+    title('RGB Normal Map');
+    axis equal;
+
+    pixel_error = normal_error(n,nn);
+    avg_error = sum(pixel_error)/size(pixel_error,1)
 end
-n_ = reshape(b,3,[])';
-len = sum(n_.*n_,2);
-len = len.^0.5;
-n = n_./[len len len];
-
-load('rabbit_small.mat');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% n = -n;
-% n(:,3) = -n(:,3);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-zz = zeros(size(nn2));
-temp = zeros(size(nn2,1),size(nn2,2));
-temp(mask) = n(:,1);zz(:,:,1) = temp;
-temp(mask) = n(:,2);zz(:,:,2) = temp;
-temp(mask) = n(:,3);zz(:,:,3) = temp;
-figure(1);
-imagesc(uint8((zz+1)*128));
-title('RGB Normal Map');
-axis equal;
-figure(2);
-imagesc(uint8((nn2+1)*128));
-title('RGB Normal Map');
-axis equal;
-
-error = normal_error(n,nn);
-sum(error)/size(error,1)
