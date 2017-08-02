21a72dc5d6ed25341098b079d75aa277aafd9491
URefrain <helei0147@163.com>
2017-08-02 16:20:11 +0800

    fill image vector to the square image

--- auto_fill_im.m
+++ auto_fill_im.m
@@ -3,13 +3,13 @@ function [rect_im] = auto_fill_im( ldr, im_mask )
 %   此处显示详细说明
     im_size = size(im_mask);
     show_im = zeros(im_size(1),im_size(2),3);
-%     show_im = uint8(show_im);
-    R_slice = zeros(im_size);
+    show_im = uint8(show_im);
+    R_slice = uint8(zeros(im_size));
     im_mask = im_mask>0;
     R_slice(im_mask) = ldr(:,1);
-    G_slice = zeros(im_size);
+    G_slice = uint8(zeros(im_size));
     G_slice(im_mask) = ldr(:,2);
-    B_slice = zeros(im_size);
+    B_slice = uint8(zeros(im_size));
     B_slice(im_mask) = ldr(:,3);
     show_im(:,:,1) = R_slice;
     show_im(:,:,2) = G_slice;
